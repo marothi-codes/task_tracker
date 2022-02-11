b18739a0f43ea88da6d354d6cc933e0c49ba5a77
Marothi Mahlake <marothi.codes@gmail.com>
2022-02-11 17:28:53 +0200

    Task and TaskItem Components written.

---
+++ src/app/mock-tasks.ts
@@ -0,0 +1,22 @@
+import {Task} from './models/Task';
+
+export const TASKS: Task[] = [
+  {
+    id: 1,
+    text: 'Doctor\'s Appointment',
+    date: '28 Feb at 14:30',
+    reminder: true
+  },
+  {
+    id: 2,
+    text: 'Angular Devs Meet and Great',
+    date: '5 Mar at 10:00',
+    reminder: true
+  },
+  {
+    id: 3,
+    text: 'Groceries and Stationary',
+    date: '5 Mar at 08:00',
+    reminder: false
+  }
+];
