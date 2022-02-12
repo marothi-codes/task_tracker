e5afa4fa3148c417056c7e0688ef8a08869f3428
Marothi Mahlake <marothi.codes@gmail.com>
2022-02-12 12:47:16 +0200

    Errors addressed, TaskService now uses Observables.

--- src/app/services/task.service.ts
+++ src/app/services/task.service.ts
@@ -1,4 +1,7 @@
 import { Injectable } from '@angular/core';
+import { Task } from '../models/Task';
+import { TASKS } from '../mock-tasks';
+import { Observable, of } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -6,4 +9,8 @@ import { Injectable } from '@angular/core';
 export class TaskService {
 
   constructor() { }
+
+  getTasks(): Observable<Task[]> {
+    return of(TASKS);
+  }
 }
